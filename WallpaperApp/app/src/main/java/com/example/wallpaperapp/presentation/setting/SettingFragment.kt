d0766c41bfd97b0b4b2298e5a624d74b9b4c4195
SabaDogonadze <sabadogonadze11@gmail.com>
2025-03-05 00:16:25 +0400

    remove read from storage logic

--- WallpaperApp/app/src/main/java/com/example/wallpaperapp/presentation/setting/SettingFragment.kt
+++ WallpaperApp/app/src/main/java/com/example/wallpaperapp/presentation/setting/SettingFragment.kt
@@ -1,16 +1,6 @@
 package com.example.wallpaperapp.presentation.setting
 
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.app.WallpaperManager
-import android.content.pm.PackageManager
-import android.graphics.drawable.Drawable
-import android.os.Build
-import android.util.Log.d
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -48,16 +38,6 @@ class SettingFragment : BaseFragment<FragmentSettingBinding>(FragmentSettingBind
     }
 
 
-  /*  private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted: Boolean ->
-        if (isGranted) {
-            loadLockScreenWallpaper()
-        } else {
-            Toast.makeText(requireContext(), "Permission denied", Toast.LENGTH_SHORT).show()
-        }
-    }*/
-
     private fun observers() {
         viewLifecycleOwner.lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
@@ -80,46 +60,11 @@ class SettingFragment : BaseFragment<FragmentSettingBinding>(FragmentSettingBind
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 settingViewModel.languageFlow.collect { language ->
                     setLocale(requireContext(), language)
-                    d("shemovida", "shemovida")
                     requireActivity().recreate()
                 }
             }
         }
     }
-
-   /* private fun checkAndRequestPermission() {
-        val permission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            Manifest.permission.READ_MEDIA_IMAGES
-        } else {
-            Manifest.permission.READ_EXTERNAL_STORAGE
-        }
-
-        if (ContextCompat.checkSelfPermission(requireContext(), permission) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissionLauncher.launch(permission)
-        } else {
-            loadLockScreenWallpaper()
-        }
-    }*/
-
-/*    @SuppressLint("MissingPermission", "NewApi", "ObsoleteSdkInt")
-    private fun loadLockScreenWallpaper() {
-        try {
-            val wallpaperManager = WallpaperManager.getInstance(requireContext())
-
-            // Get Lock Screen wallpaper (API 24+)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                val lockScreenDrawable: Drawable? = wallpaperManager.getDrawable(WallpaperManager.FLAG_LOCK)
-                binding.ivLockScreen.setImageDrawable(lockScreenDrawable)
-            }
-
-            // Get Home Screen wallpaper
-            val homeScreenDrawable: Drawable? = wallpaperManager.drawable
-            binding.ivHomeScreen.setImageDrawable(homeScreenDrawable)
-
-        } catch (e: Exception) {
-            Toast.makeText(requireContext(), "Failed to load wallpapers", Toast.LENGTH_SHORT).show()
-        }
-    }*/
 
     private fun openLogInFragment() {
         findNavController().navigate(SettingFragmentDirections.actionSettingFragmentToLoginFragment2())
